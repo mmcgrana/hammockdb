64715293cfb29f59c00c2c8cc97dc1ce49dc0858
Mark McGranaghan <mmcgrana@gmail.com>
2010-11-02 07:36:26 -0700

    starting to see some abstractions for hammockdb.data

--- src/hammockdb/data.clj
+++ src/hammockdb/data.clj
@@ -1,6 +1,18 @@
 (ns hammockdb.data
   (:import java.util.UUID))
 
+(defn write-fn [pure-write]
+  (fn [state & args]
+    (let [state* @astate
+          [state** ret] (apply write-fn @state args)]
+      (when state**
+        (assert (compare-and-set! state state* state**)))
+      ret)))
+
+(defn read-fn [pure-read]
+  (fn [state & args]
+    (apply pure-read @state args)))
+
 (defn uuid []
   (str (UUID/randomUUID)))
 
@@ -15,54 +27,45 @@
 (defn db-new [dbid]
   {})
 
-(defn db-create* [state* dbid]
+(defn db-create*
+  "existing-db, db"
+  [state* dbid]
   (if (get-in state* [:dbids dbid])
     [nil {:existing-db true}]
     (let [db (db-new dbid)]
       [(assoc-in state* [:dbs dbid] db) {:db db}])))
 
-(defn db-create
-  "existing-db, db"
-  [state dbid]
-  (let [state* @astate
-        [state** ret] (db-create* @state dbid)]
-    (when state**
-      (assert (compare-and-set! state state* state**)))
-    ret))
+(def db-create (write-fn db-create*))
 
-(defn- db-meta [db dbid]
+(defn db-inflate [db dbid]
   {"db_name" dbid
    "doc_count" (count db)
    "doc_size" (* 339.2 (count db))})
 
-(defn db-get* [state* dbid]
+(defn db-get*
+  "no-db, db"
+  [state* dbid]
   (if-not-let [db (get-in state* [:dbs dbid])]
     {:no-db true}
-    {:db (db-meta db dbid)}))
+    {:db (db-inflate db dbid)}))
 
-(defn db-get
-  "no-db, db"
-  [state dbid]
-  (db-get* @state dbid))
+(def db-get (read-fn db-get*))
 
-(defn db-delete* [state* dbid]
+(defn db-delete*
+  "no-db, ok"
+  [state* dbid]
   (if-not-let [db (get-in state* [:dbs dbid])]
     [nil {:no-db true}]
     [(dissoc state* :dbs dbid) {:ok true}]))
 
-(defn db-delete
-  "no-db, ok"
-  [state dbid]
-  (let [state* @astate
-        [state** ret] (db-delete* @state dbid)]
-    (when state**
-      (assert (compare-and-set! state state* state**)))
-    ret))
+(def db-delete (write-fn db-delete*))
 
-(defn- doc-inflate [raw-doc opts]
+(defn doc-inflate [raw-doc opts]
   raw-doc)
 
-(defn doc-get [state dbid docid & [opts]]
+(defn doc-get*
+  "no-db, no-doc, del-doc, doc"
+  [state* dbid doc-id & [opts]]
   (if-not-let [db (get-in @state [:dbs dbid])]
     {:no-db true}
     (if-not-let [doc (get db docid)]
@@ -71,24 +74,32 @@
         {:del-doc true}
         {:doc (doc-inflate doc opts)}))))
 
-(defn doc-put
-  "no-db, bad-doc, doc"
-  [state dbid docid doc & [opts]]
-  (if-not-let [db (get-in @state [:dbs dbid])]
-    {:no-db true}
+(def doc-get (read-fn doc-get*))
 
-(defn doc-post
+(defn doc-put*
   "no-db, bad-doc, doc"
-  [state dbid doc]
+  [state* dbid docid doc & [opts]]
+  (if-not-let [db (get-in state* [:dbs dbid])]
+    {:no-db true}))
+
+(def doc-put (write-fn doc-put*))
+
+(defn doc-post*
+  "no-db, bad-doc, doc"
+  [state* dbid doc]
   (let [docid (or (get doc "_id") (uuid))
         doc (assoc doc "_id" docid)]
-    (doc-put state dbid docid doc)))
+    (doc-put* state* dbid docid doc)))
 
-(defn doc-delete
+(def doc-post (write-fn doc-post*))
+
+(defn doc-delete*
   "no-db, no-doc, doc"
-  [state dbid docid rev]
-  (if-not-let [db (get-in @state [:dbs dbid])]
+  [state* dbid docid rev]
+  (if-not-let [db (get-in state* [:dbs dbid])]
     {:no-db true}
     (if-not-let [doc (get db docid)]
       {:no-doc true}
       ())))
+
+(def doc-delete (write-fn doc-delete*))
